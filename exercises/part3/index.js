c1deb715e032b92c0e21dd43f3e5874bac92a992
Yufei Cui <yufei.cui@mail.utoronto.ca>
2021-08-01 00:52:12 -0400

    finish part 3a

---
+++ exercises/part3/index.js
@@ -0,0 +1,118 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+
+morgan.token('post', function getId (req) {
+  if (req.method == 'POST') {
+    return JSON.stringify(req.body)
+  }
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
+
+let persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+const peopleCount = persons.length
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/info', (req, res) => {
+  const info = `<p>Phonebook has info for ${peopleCount} people</p>`
+  const date = `<p>${new Date()}</p>`
+  res.send(info + date)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person == undefined) {
+    return res.status(404).send(`There is no note with the id ${req.params.id}`)
+  }
+
+  res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person == undefined) {
+    return res.status(404).send(`There is no note with the id ${req.params.id}`)
+  }
+
+  // delete from the server db
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+const generateID = () => {
+  const maxID = persons.length > 0
+  // use ... notes.map() b/c Math.max takes numbers not list
+  ? Math.max(...persons.map(person => person.id))
+  : 0
+
+  return maxID + 1
+}
+
+const isPersonDuplicate = (name) => {
+  return persons.find(person => person.name === name) != undefined
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'Missing name or number!'
+    })
+  }
+
+  if (isPersonDuplicate(body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique!'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateID()
+  }
+
+  // add to the server db
+  persons = persons.concat(person)
+
+  res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
---
+++ part3/index.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+
+// Without the json-parser, the req.body property would be undefined.
+// The json-parser functions so that it takes the JSON data of a request,
+// transforms it into a JavaScript object and then attaches it to the
+// [body property of the request object] before the route handler is called.
+app.use(express.json())
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true
+  }
+]
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const found = notes.find(note => note.id === id)
+  if (found != undefined) {
+    res.json(found)
+  } else {
+    res.status(404).send(`There is no note with the id ${req.params.id}`)
+  }
+})
+
+const generateID = () => {
+  const maxID = notes.length > 0
+    // use ... notes.map() b/c Math.max takes numbers not list
+    ? Math.max(...notes.map(note => note.id))
+    : 0
+
+  return maxID + 1
+}
+
+app.post('/api/notes/', (req, res) => {
+  const body = req.body
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: 'content missing!'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateID()
+  }
+
+  // add note to the server db
+  notes = notes.concat(note)
+
+  res.json(note)
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  // pretend delete
+  notes.filter(note => note.id !== id)
+
+  res.status(204).end()
+})
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+// if req didnt hit any of the endpoints then it will trigger "unknownEndpoint"
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
